71e62fde60ec073df7947528a3071f4a02563aeb
David Dada <daveproxy80@gmail.com>
2024-02-26 19:20:03 +0100

    add NFTMarketplace

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.21;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    address public owner;
+    uint256 public tokenId;
+    mapping(uint256 => uint256) public tokenPrices;
+
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        owner = msg.sender;
+    }
+
+    function mintNFT(address _to, string memory _tokenURI, uint256 _price) external {
+        tokenId++;
+        tokenPrices[tokenId] = _price;
+        _mint(_to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+    }
+
+    function buyNFT(uint256 _tokenId) external payable {
+        require(msg.value >= tokenPrices[_tokenId], "NFTMarketplace: value must be greater than or equal to token price");
+        address seller = ownerOf(_tokenId);
+        address buyer = msg.sender;
+        uint256 price = msg.value;
+
+        safeTransferFrom(seller, buyer, _tokenId);
+        payable(seller).transfer(price);
+    }
+}
